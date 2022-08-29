2613d21519b3573d5580c447c2826d88e1ea4499
abbisatria <abbisatria48@gmail.com>
2022-08-29 18:18:15 +0700

    slicing

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "react-datepicker/dist/react-datepicker.css"
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
